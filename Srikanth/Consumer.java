818f58ecfd7b55bad6de60b7bc2c3103609af323
Naga Ineni <naagachowdary@gmail.com>
2013-12-10 15:51:18 +0530

    Update Consumer.java

--- Srikanth/Consumer.java
+++ Srikanth/Consumer.java
@@ -19,6 +19,7 @@ import org.apache.hadoop.util.GenericOptionsParser;
 public class Consumer {
 
 	/**
+	 * This is used to extract the required fields from Consumer Logs..... like messageid, timestamp, message topic...
 	 * @param args
 	 * @throws IOException 
 	 * @throws InterruptedException 
