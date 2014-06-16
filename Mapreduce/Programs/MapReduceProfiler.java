18de6d1729cbc83666bd0d04416178009d413a38
Naga Ineni <naagachowdary@gmail.com>
2014-06-16 15:17:06 +0530

    Update MapReduceProfiler.java

--- Mapreduce/Programs/MapReduceProfiler.java
+++ Mapreduce/Programs/MapReduceProfiler.java
@@ -22,63 +22,62 @@ public class MapReduceProfiler {
 * @throws IOException
 */
 
-public static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable>{
-public void map(LongWritable key, Text value, Context context) throws InterruptedException, IOException
-{
-String line = value.toString();
-String[] parts = line.split("\\t");
-if(parts.length == 9)
-{
-String stockName = parts[1];
-long volume = Long.valueOf(parts[7]);
-context.write(new Text(stockName), new LongWritable(volume));
-}
-}
-}
+  public static class MyMapper extends Mapper<LongWritable, Text, Text, LongWritable>
+  {
+      public void map(LongWritable key, Text value, Context context) throws InterruptedException, IOException
+      {
+          String line = value.toString();
+          String[] parts = line.split("\\t");
+          if(parts.length == 9)
+          {
+              String stockName = parts[1];
+              long volume = Long.valueOf(parts[7]);
+              context.write(new Text(stockName), new LongWritable(volume));
+          }
+      }
+  }
+  public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable>
+  {
+      public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException
+      {
+        long sum = 0;
+        for(LongWritable value : values)
+        {
+            sum = sum + value.get();
+        }
+        context.write(key, new LongWritable(sum));
+      }
+  }
 
-public static class MyReducer extends Reducer<Text, LongWritable, Text, LongWritable>
-{
-public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException
-{
-long sum = 0;
-for(LongWritable value : values)
-{
-sum = sum + value.get();
-}
-context.write(key, new LongWritable(sum));
-}
-}
+  public static void main(String args[]) throws IOException, InterruptedException, ClassNotFoundException
+  {
+      Configuration conf = new Configuration();
 
-public static void main(String args[]) throws IOException, InterruptedException, ClassNotFoundException
-{
-Configuration conf = new Configuration();
+      //Enable JVM Profiler, the profiler name is HPROF Profiler.......
+      conf.setBoolean("mapreduce.task.profile", true);
+      conf.set("mapreduce.task.profile.params", "-agentlib:hprof=cpu=samples,heap=sites,depth=6,force=n,thread=y,verbose=n,file=%s");
+      conf.set("mapreduce.task.profile.maps", "0-2");
+      conf.set("mapreduce.task.profile.reduces", "");
 
 
-//Enable JVM Profiler, the profiler name is HPROF Profiler.......
-conf.setBoolean("mapreduce.task.profile", true);
-conf.set("mapreduce.task.profile.params", "-agentlib:hprof=cpu=samples,heap=sites,depth=6,force=n,thread=y,verbose=n,file=%s");
-conf.set("mapreduce.task.profile.maps", "0-2");
-conf.set("mapreduce.task.profile.reduces", "");
+      job job = new Job(conf, "Finding the stock Volume");
+      job.setJarByClass(MapReduceProfiler.class);
 
+      job.setMapperClass(MyMapper.class);
+      job.setCombinerClass(MyReducer.class);
+      job.setReducerClass(MyReducer.class);
 
-Job job = new Job(conf, "Finding the stock Volume");
-job.setJarByClass(MapReduceProfiler.class);
+      job.setMapOutputKeyClass(Text.class);
+      job.setMapOutputValueClass(LongWritable.class);
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(LongWritable.class);
 
-job.setMapperClass(MyMapper.class);
-job.setCombinerClass(MyReducer.class);
-job.setReducerClass(MyReducer.class);
+      job.setInputFormatClass(TextInputFormat.class);
+      job.setOutputFormatClass(TextOutputFormat.class);
 
-job.setMapOutputKeyClass(Text.class);
-job.setMapOutputValueClass(LongWritable.class);
-job.setOutputKeyClass(Text.class);
-job.setOutputValueClass(LongWritable.class);
+      FileInputFormat.addInputPath(job, new Path(args[0]));
+      FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-job.setInputFormatClass(TextInputFormat.class);
-job.setOutputFormatClass(TextOutputFormat.class);
-
-FileInputFormat.addInputPath(job, new Path(args[0]));
-FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-System.exit(job.waitForCompletion(true) ? 0 : 1);
-}
+      System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
 }
