a5409395da8f3d06e5547ca504d6aef19d794771
Naga Ineni <naagachowdary@gmail.com>
2014-11-18 15:42:18 +0530

    Update reducer.R

--- Streaming/R/reducer.R
+++ Streaming/R/reducer.R
@@ -1,7 +1,28 @@
 #!/usr/bin/env Rscript
-f <- file("stdin")
-open(f)
-while(length(line <- readLines(f,n=1)) > 0) {
-	key <-  as.numeric(trim(strsplit(line, split="\t")[[1]][[2]]))	
-  #write(key, stderr())
+
+trimWhiteSpace <- function(line) gsub("(^ +)|( +$)", "", line)
+
+splitLine <- function(line) {
+  val <- unlist(strsplit(line, "\t"))
+  list(word = val[1], count = as.integer(val[2]))
 }
+
+env <- new.env(hash = TRUE)
+
+con <- file("stdin", open = "r")
+while (length(line <- readLines(con, n = 1, warn = FALSE)) > 0) {
+  line <- trimWhiteSpace(line)
+  split <- splitLine(line)
+  word <- split$word
+  count <- split$count
+  if (exists(word, envir = env, inherits = FALSE)) {
+      oldcount <- get(word, envir = env)
+      assign(word, oldcount + count, envir = env)
+  }
+  else assign(word, count, envir = env)
+}
+close(con)
+
+for (w in ls(env, all = TRUE))
+  cat(w, "\t", get(w, envir = env), "\n", sep = "")
+
