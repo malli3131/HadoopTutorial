e4197bcc00008f0290eba90c04127ee293fafdba
naga inelu <naagachowdary@gmail.com>
2018-12-12 16:51:29 +0530

    Update ReadHDFS.java

--- Hdfs/Java HDFS API/ReadHDFS.java
+++ Hdfs/Java HDFS API/ReadHDFS.java
@@ -14,29 +14,12 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 
 public class ReadHDFS {
-
-	/**
-	 * @param args
-	 * @author Nagamallikarjuna
-	 * @throws URISyntaxException 
-	 * @throws IOException 
-	 */
 	public static void main(String[] args) throws IOException, URISyntaxException {
 		Configuration conf = new Configuration();
 		Path fileName = new Path(args[0]);
-		Pattern pattern = Pattern.compile("^part.*");
-		Matcher matcher = null;
 		FileSystem hdfs = FileSystem.get(new URI("hdfs://localhost:9000"), conf);
-		FileStatus files[] = hdfs.listStatus(fileName);
-		for(FileStatus file: files)
-		{
-			matcher = pattern.matcher(file.getPath().getName());
-			if(matcher.matches())
-			{
-				FSDataInputStream in = hdfs.open(file.getPath());
-				in.seek(0);
-				IOUtils.copyBytes(in, System.out, conf, false);
-			}
-		}
+		FSDataInputStream in = hdfs.open(fileName);
+		in.seek(0);
+		IOUtils.copyBytes(in, System.out, conf, false);
 	}
 }
